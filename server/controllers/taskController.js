1f813785a12caefad589ac4db0e31de752474c93
KrhystFerrari <krhyst_ferrari@hotmail.com>
2024-09-02 18:50:38 -0300

    feat: backend para o CRUD criado

---
+++ server/controllers/taskController.js
@@ -0,0 +1,38 @@
+const Task = require('../models/Task');
+
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.createTask = async (req, res) => {
+  const newTask = new Task(req.body);
+  try {
+    const savedTask = await newTask.save();
+    res.status(201).json(savedTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Task deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
